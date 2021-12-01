3b178f52f92e7a9970371d70dac9bc29904d8e6a
mridenour7 <82553574+mridenour7@users.noreply.github.com>
2021-12-01 00:56:32 -0800

    Create dataCleaner.py
    Data preprocessing

---
+++ dataCleaner.py
@@ -0,0 +1,66 @@
+# Import libraries
+import pyspark
+from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
+from pyspark.context import SparkContext
+
+# Create spark context
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
+sql = SQLContext(sc)
+
+# Create dataframes
+df_highway = (sql.read
+         .format("com.databricks.spark.csv")
+         .option("header", "true")
+         .load("highways.csv"))
+
+df_detector = (sql.read
+         .format("com.databricks.spark.csv")
+         .option("header", "true")
+         .load("freeway_detectors.csv"))
+
+df_station = (sql.read
+         .format("com.databricks.spark.csv")
+         .option("header", "true")
+         .load("freeway_stations.csv"))
+
+df_loopdata = (sql.read
+         .format("com.databricks.spark.csv")
+         .option("header", "true")
+         .load("freeway_loopdata.csv"))
+
+# Drop unwanted columns
+df_detector = df_detector.drop("milepost", "detectorclass","lanenumber")
+
+df_station = df_station.drop("milepost","numberlanes","latlon","stationclass")
+
+df_highway = df_highway.drop("direction")
+
+df_loopdata = df_loopdata.drop("occupancy","status","dqflags")
+
+# Join the dataframes
+df_first = df_highway.join(df_detector, on=['highwayid'], how='full')
+
+# Drop highwayid to avoid duplicate
+df_first = df_first.drop("highwayid")
+df_first = df_first.drop("locationtext")
+
+# Join to get the final result
+df_second = df_first.join(df_station, on=['stationid'], how='full')
+df_third = df_second.join(df_loopdata, on=['detectorid'], how='full')
+df_third.cache()
+
+from pyspark.sql.functions import regexp_replace
+df_third = df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " (N|E|S|W)B", "")) # remove directions
+df_third = df_third.withColumn("locationtext", regexp_replace(df_third['locationtext'], " to ?", "")) # remove " to " and " to"
+df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " at ", ""))
+df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], "I-205", ""))
+df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " Blvd", ""))
+df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " Cr", " Creek")) # expand CR to Creek
+# Show the result
+df_third.show()
+df_third = df_third.na.drop('any')
+df_third.show()
+# Convert to CSV
+df_third.coalesce(1).write.option("header", "true").csv("processed_data")
