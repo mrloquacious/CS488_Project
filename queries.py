354828f5cdd976550a864842dfcdd9854d4dd8ff
Sean Humm <hummsean@pdx.edu>
2021-12-02 14:31:06 -0800

    small formatting changes. Included Jesse's queries

--- queries.py
+++ queries.py
@@ -1,17 +1,20 @@
 import pyspark
+from pyspark.sql.functions import substring, length, col, expr, to_timestamp
 from pyspark.sql import functions as F
 from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
+from pprint import pprint   
 
-spark = SparkSession.builder.getOrCreate() sc = spark.sparkContext
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
 sql = SQLContext(sc)
 
 # Create dataframes
 df_highway = (sql.read
          .format("com.databricks.spark.csv")
          .option("header", "true")
-         .load("gs://trafficdata_f21/processed_data/trafficData.csv")
+         .load("gs://trafficdata_f21/processed_data.csv")
          )
 df_highway.cache()
 
@@ -27,7 +30,7 @@ print("\nQuery #2, total volume for the station Foster NB for Sept 15, 2011:")
 # WHERE locationtext == Foster
 # AND shortdirection == N
 # AND startime = Sept 15 (All Day)
-volume = df_highway.select(df_highway.volume).filter((df_highway.locationtext == "Foster")
+volume = df_highway.filter((df_highway.locationtext == "Foster")
     & (df_highway.shortdirection=='N')
     & (df_highway.starttime > "2011-10-15 00:00:00-07")
     & (df_highway.starttime < "2011-10-16 00:00:00-07")).agg(F.sum(df_highway.volume))
@@ -35,13 +38,59 @@ volume = df_highway.select(df_highway.volume).filter((df_highway.locationtext ==
 print("Volume for Foster NB on Sept. 15, 2011:\n")
 volume.show()
 
-'''Query #5'''
-'''7am-9am'''
-print("\nQuery #5, average travel time for 7-9AM and 4-6PM on September 22, 2011 for the I-205 NB freeway:")
+'''Query #3'''
+# Convert starttime to timestamp:
+df3 = df_highway.withColumn("starttime", expr("substring(starttime, 1, length(starttime)-3)") .cast('timestamp'))
 
+# Create a temp view to use in queries:
+df3.createOrReplaceTempView("df4")
+
+# Filter the Foster Rd NB data for 9/15/11
+df5 = spark.sql('''SELECT * FROM df4 WHERE locationtext = 'Foster'
+                AND starttime > '2011-09-15 00:00:05' 
+                AND starttime <= '2011-09-16 00:00:00'
+                AND shortdirection = 'N' 
+                AND volume > 0 AND speed > 0''')
+
+# Create temp view of records for 9/15/11:
+df5.createOrReplaceTempView("df_day")
+
+#Query the length of the Foster NB station:
+query_len = ''' SELECT DISTINCT length FROM df_day
+        WHERE locationtext = "Foster" AND
+        shortdirection = "N" '''
+len_df = spark.sql(query_len)
+station_len = len_df.collect()[0][0]
+
+# Create dictionary to hold result data:
+travel_times = {}
+
+# Loop over data in 5 minute increments:
+for i in range(0, 120, 5):
+    # Query to get average speed and end time for 5 minute period:
+    query_5mins = f'''SELECT starttime, speed from df_day
+                WHERE starttime > "2011-09-15 00:00:00" + INTERVAL {i} minutes
+                AND starttime <= "2011-09-15 00:00:00" + INTERVAL {i + 5} minutes '''
+    # Get the dataframe for the 5 minute period:
+    rows = spark.sql(query_5mins)
+    # Create temp view for the period:
+    rows.createOrReplaceTempView("df_5min")
+    # Extract the last timestamp for the 5 minute period and the average speed:
+    endtime = rows.agg({'starttime': 'max'})
+    avg = rows.agg({'speed': 'avg'})
+
+    travel_times[str(endtime.collect()[0][0])] = \
+        (float(station_len) / avg.collect()[0][0]) * 3600
+
+pprint(travel_times)
+
+'''Query #5'''
+
+print("\nQuery #5, average travel time for 7-9AM and 4-6PM on September 22, 2011 for the I-205 NB freeway:")
+'''7am-9am'''
 #Filter df so we work with only the data we need
-filteredDf = df_highway.filter((df_highway.starttime > "2011-10-11 07:00:00-07") 
-                              & (df_highway.starttime < "2011-10-11 09:00:00-07")
+filteredDf = df_highway.filter((df_highway.starttime > "2011-10-22 07:00:00-07") 
+                              & (df_highway.starttime < "2011-10-22 09:00:00-07")
                               & (df_highway.highwayname=="I-205") 
                               & (df_highway.shortdirection=='N'))
 
@@ -81,4 +130,49 @@ df_travelTime = spark.createDataFrame([R6(a,d) for a,b,c,d in travelTime])
 
 #Sum Travel Time column to find total travel time
 print("Travel time: \n")
+df_travelTime.agg(F.sum("Travel Time for 7am-9am")).show()
+
+'''4pm-6pm (16:00-18:00)'''
+#Filter df so we work with only the data we need
+afternoonFilteredDf = df_highway.filter((df_highway.starttime > "2011-10-22 16:00:00-07") 
+                              & (df_highway.starttime < "2011-10-22 18:00:00-07")
+                              & (df_highway.highwayname=="I-205") 
+                              & (df_highway.shortdirection=='N'))
+
+afternoonFilteredDf.cache()
+#Determine avg detector speed
+avgDetectorSpeeds = afternoonFilteredDf.select('detectorid', 'speed').groupBy('detectorid').agg(F.avg('speed')).collect()
+#Convert from list to df
+R = Row('detectorid', 'speed')
+#Create df
+df_avgDetectorSpeed = spark.createDataFrame([R(x,y) for x, y in avgDetectorSpeeds])
+
+#Determine which detectors are associated with which stations
+stationDetectors = afternoonFilteredDf.select('stationid', 'detectorid').distinct().collect()
+R2 = Row('stationid', 'detectorid')
+df_stationDetectors = spark.createDataFrame([R2(x,y) for x, y in stationDetectors])
+
+#Join df_stationDetectors with df_avgDetectorSpeed on detectorid to get new table
+stationDetectorSpeed = df_stationDetectors.join(df_avgDetectorSpeed, ['detectorid']).collect()
+R3 = Row('stationid', 'detectorid', 'speed')
+df_stationDetectorSpeed = spark.createDataFrame([R3(y, x, z) for x, y, z in stationDetectorSpeed])
+
+#Find avgStationSpeed
+avgStationSpeed = df_stationDetectorSpeed.groupBy('stationid').agg(F.avg('speed')).collect()
+R4 = Row('stationid', 'speed')
+df_avgStationSpeed = spark.createDataFrame([R4(x,y) for x, y in avgStationSpeed])
+
+#Find stationLengths
+#Since each station should have only 1 length, we look for distinct stationid and include the length. 
+stationLengths = afternoonFilteredDf.select('stationid', 'length').distinct().collect()
+R5 = Row('stationid', 'length')
+df_stationLengths = spark.createDataFrame([R5(x,y) for x, y in stationLengths])
+
+#Create final dataframe that shows the travel time to each station
+travelTime = df_avgStationSpeed.join(df_stationLengths, ['stationid']).withColumn("Travel Time", (F.col('length')/F.col('speed'))*60).collect()
+R6 = Row('stationid', 'Travel Time')
+df_travelTime = spark.createDataFrame([R6(a,d) for a,b,c,d in travelTime])
+
+#Sum Travel Time column to find total travel time
+print("Travel time for 4pm-6pm: \n")
 df_travelTime.agg(F.sum("Travel Time")).show()
