84a5d0195df2cdd8464a8daac2d50ac2fd6cf039
mridenour7 <82553574+mridenour7@users.noreply.github.com>
2021-12-02 02:09:24 -0800

    Update dataCleaner.py

--- dataCleaner.py
+++ dataCleaner.py
@@ -37,7 +37,7 @@ df_station = df_station.drop("milepost","numberlanes","latlon","stationclass")
 
 df_highway = df_highway.drop("direction")
 
-df_loopdata = df_loopdata.drop("occupancy","status","dqflags")
+df_loopdata = df_loopdata.drop("occupancy","status")
 
 # Join the dataframes
 df_first = df_highway.join(df_detector, on=['highwayid'], how='full')
@@ -49,18 +49,22 @@ df_first = df_first.drop("locationtext")
 # Join to get the final result
 df_second = df_first.join(df_station, on=['stationid'], how='full')
 df_third = df_second.join(df_loopdata, on=['detectorid'], how='full')
-df_third.cache()
 
 from pyspark.sql.functions import regexp_replace
 df_third = df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " (N|E|S|W)B", "")) # remove directions
 df_third = df_third.withColumn("locationtext", regexp_replace(df_third['locationtext'], " to ?", "")) # remove " to " and " to"
-df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " at ", ""))
-df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], "I-205", ""))
-df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " Blvd", ""))
-df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " Cr", " Creek")) # expand CR to Creek
+df_third = df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " at ", ""))
+df_third = df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], "I-205", ""))
+df_third = df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " Blvd", ""))
+df_third = df_third.withColumn("locationtext", regexp_replace(df_third["locationtext"], " Cr", " Creek")) # expand CR to Creek
+
+df_third = df_third.filter(df_third.dqflags.cast('int').bitwiseAND(16) == 0) # only keep row if bit position 5 in the dqflag is off (on means Speed = 0 and Volume > 0)
+df_third = df_third.filter(df_third.dqflags.cast('int').bitwiseAND(32) == 0) # bit position 6 in the dqflag is off (on means Speed > 0 and Volume = 0)
+df_third = df_third.filter(df_third.dqflags.cast('int').bitwiseAND(64) == 0) # bit position 7 in the dqflag is off (on means Occupancy > 0 and Volume = 0)
+
+
+
 # Show the result
-df_third.show()
-df_third = df_third.na.drop('any')
 df_third.show()
 # Convert to CSV
 df_third.coalesce(1).write.option("header", "true").csv("processed_data")
